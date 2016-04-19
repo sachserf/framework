10057376510b9658dcc6375ed6a85bba3a951c4a
Frederik Sachser <f.sachser@gmx.at>
2016-04-19 22:10:14 +0200

    test dput

--- R/project_skeleton.R
+++ R/project_skeleton.R
@@ -15,8 +15,24 @@ project_skeleton <-
     } else {
       warning("input/R/clean.R already exists")
     }
+    dput_function <- function(pkg_fun, target_dir, substitute_framework = FALSE) {
+      # extract name info
+      function_name <- paste(substitute(pkg_fun))[3]
+      function_call <- paste0(function_name, ' <- ')
+      filename <- paste0(target_dir, '/', function_name, '.R')
+      # write file
+      dput(x = pkg_fun, file = filename)
+      # read file
+      thefun <- readLines(filename)
+      # substitute framework:: with local_fun$
+      if (substitute_framework == TRUE) {
+        thefun <- gsub(pattern = 'framework::', replacement = 'local_fun$', x = thefun)
+      }
+      # include function_call and overwrite file 
+      cat(function_call, thefun, sep = '\n', file = filename)
+    }
     # create pkg_install-function
-      framework::dput_function(pkg_fun = framework::git_init, 
+      dput_function(pkg_fun = framework::git_init, 
                                target_dir = 'input/functions/sachserf/framework', 
                                substitute_framework = TRUE)
     # create write_dataframe-function
