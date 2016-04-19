5d7d8e1c651b99f50bbc7db42f69cc878c82c991
Frederik Sachser <f.sachser@gmx.at>
2016-04-19 22:18:12 +0200

    dput test

--- R/dput_function.R
+++ R/dput_function.R
@@ -1,18 +1,30 @@
-dput_function <- function(pkg_fun, target_dir, substitute_framework = FALSE) {
-  # extract name info
-  function_name <- paste(substitute(pkg_fun))[3]
-  function_call <- paste0(function_name, ' <- ')
-  filename <- paste0(target_dir, '/', function_name, '.R')
-  # write file
-  dput(x = pkg_fun, file = filename)
-  # read file
-  thefun <- readLines(filename)
-  # substitute framework:: with local_fun$
-  if (substitute_framework == TRUE) {
-    thefun <- gsub(pattern = 'framework::', replacement = 'local_fun$', x = thefun)
+dput_function <-
+  function(pkg_fun,
+           target_dir,
+           substitute_framework = FALSE) {
+    if (dir.exists(target_dir) == FALSE) {
+      dir.create(target_dir, recursive = TRUE)
+    }
+    # extract name info
+    function_name <- paste(substitute(pkg_fun))[3]
+    function_call <- paste0(function_name, ' <- ')
+    filename <- paste0(target_dir, '/', function_name, '.R')
+    # check if file exists
+    if (file.exists(filename) == TRUE) {
+      warning('filename of function already exists.')
+    } else {
+      # write file
+      dput(x = pkg_fun, file = filename)
+      # read file
+      thefun <- readLines(filename)
+      # substitute framework:: with local_fun$
+      if (substitute_framework == TRUE) {
+        thefun <-
+          gsub(pattern = 'framework::',
+               replacement = 'local_fun$',
+               x = thefun)
+      }
+      # include function_call and overwrite file
+      cat(function_call, thefun, sep = '\n', file = filename)
+    }
   }
-  # include function_call and overwrite file 
-  cat(function_call, thefun, sep = '\n', file = filename)
-}
-
-
