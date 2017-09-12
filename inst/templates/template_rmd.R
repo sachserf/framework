8db528eccec5b305789a42f2dfcf25b64f69ab40
Frederik Sachser <f.sachser@gmx.at>
2017-09-12 16:27:55 +0200

    update template_rmd cc000

--- inst/templates/template_rmd.R
+++ inst/templates/template_rmd.R
@@ -17,6 +17,7 @@
 #' @param open Logical. If TRUE the file will be opened (via `file.edit``).
 #' @param doctype Character. Specify file extension for different output formats
 #'   of Rmd-files (pdf, html, docx).
+#' @param knitr_sidecar Logical. Write sidecar file for knitr-setup?
 #' @note Missing directories will be created recursively.
 #' @note It is not possible to overwrite existing files.
 #' @note Other YAML header options will be choosen automatically. Edit the
@@ -31,12 +32,13 @@ template_rmd <-
            Author = Sys.info()["effective_user"],
            Date = "`r Sys.Date()`",
            open = TRUE,
-           doctype = "html")
+           doctype = "html",
+           knitr_sidecar = TRUE)
   {
     if (dir.exists(paths = dirname(file)) == FALSE) {
       dir.create(path = dirname(file), recursive = TRUE)
     }
-
+    
     # check file extension
     if (tolower(substr(
       x = basename(file),
@@ -48,7 +50,7 @@ template_rmd <-
         substr(x = basename(file),
                start = 1,
                stop = nchar(basename(file)) - 4)
-     # file <- paste0(header, ".Rmd")
+      # file <- paste0(header, ".Rmd")
     } else if (toupper(substr(
       x = basename(file),
       start = nchar(basename(file)) - 1,
@@ -59,7 +61,7 @@ template_rmd <-
         substr(x = basename(file),
                start = 1,
                stop = nchar(basename(file)) - 2)
-    #  file <- paste0(header, ".R")
+      #  file <- paste0(header, ".R")
     } else if (tools::file_path_sans_ext(file) == file.path(file)) {
       fileext <- ".R"
       header <- basename(file)
@@ -69,25 +71,34 @@ template_rmd <-
       file.edit(file)
       return(message("Unknown file extension. Using file.edit()"))
     }
-
-yaml_r_docx <- paste0("#' ---\n#' title: '", header,"'\n#' author: '", Author, "'\n#' date: '", Date, "'\n#' output:\n#'  word_document: default\n#' ---")
-
-yaml_r_html <- paste0("#' ---\n#' title: '", header,"'\n#' author: '", Author, "'\n#' date: '", Date, "'\n#' output:\n#'  html_document:\n#'    theme: journal\n#'    highlight: tango\n#'    df_print: kable\n#'    fig_caption: yes\n#'    number_sections: yes\n#'    collapsed: yes\n#'    code_folding: hide\n#'    toc: yes\n#'    toc_float: yes\n#'    toc_depth: 5\n#'     keep_md: yes\n#' ---")
-
-yaml_r_pdf <- paste0("#' ---\n#' title: '", header,"'\n#' author: '", Author, "'\n#' date: '", Date, "'\n#' output:\n#'   pdf_document:\n#'     highlight: tango\n#'     df_print: kable\n#'     fig_caption: yes\n#'     number_sections: yes\n#'     toc: yes\n#'     toc_depth: 5\n#'     keep_tex: yes\n#'     latex_engine: xelatex\n#' mainfont: 'Arial'\n#' linkcolor: 'blue'\n#' urlcolor: 'blue'\n#' citecolor: 'blue'\n#' fontsize: 12pt\n#' geometry: margin=1in\n#' classoption: oneside\n#' toc: yes\n#' lot: yes\n#' lof: yes\n#' # bibliography: path2.bib\n#' link-citations: yes\n#' # csl: path2.csl # https://github.com/citation-style-language/styles\n#' ---")
-
+    
+    yaml_r_docx <- paste0("#' ---\n#' title: '", header,"'\n#' author: '", Author, "'\n#' date: '", Date, "'\n#' output:\n#'  word_document: default\n#' ---")
+    
+    yaml_r_html <- paste0("#' ---\n#' title: '", header,"'\n#' author: '", Author, "'\n#' date: '", Date, "'\n#' output:\n#'  html_document:\n#'    theme: journal\n#'    highlight: tango\n#'    df_print: kable\n#'    fig_caption: yes\n#'    number_sections: yes\n#'    collapsed: yes\n#'    code_folding: hide\n#'    toc: yes\n#'    toc_float: yes\n#'    toc_depth: 5\n#'    keep_md: yes\n#' ---")
+    
+    yaml_r_pdf <- paste0("#' ---\n#' title: '", header,"'\n#' author: '", Author, "'\n#' date: '", Date, "'\n#' output:\n#'   pdf_document:\n#'     highlight: tango\n#'     df_print: kable\n#'     fig_caption: yes\n#'     number_sections: yes\n#'     toc: yes\n#'     toc_depth: 5\n#'     keep_tex: yes\n#'     latex_engine: xelatex\n#' mainfont: 'Arial'\n#' linkcolor: 'blue'\n#' urlcolor: 'blue'\n#' citecolor: 'blue'\n#' fontsize: 12pt\n#' geometry: margin=1in\n#' classoption: oneside\n#' toc: yes\n#' lot: yes\n#' lof: yes\n#' # bibliography: path2.bib\n#' link-citations: yes\n#' # csl: path2.csl # https://github.com/citation-style-language/styles\n#' ---")
+    
     yaml_rmd_html <- gsub("#' ", "", yaml_r_html)
     yaml_rmd_docx <- gsub("#' ", "", yaml_r_docx)
     yaml_rmd_pdf <- gsub("#' ", "", yaml_r_pdf)
-
-    body_r <- paste0("knitr::opts_chunk$set(dev = 'png', dpi = 100, error = TRUE, warning = TRUE, message = TRUE, echo = TRUE)\nunlink('", paste0(header, "_warnings.Rout"), "')\nknit_hook_stderr <- function(x, options) {\n  writeLines(text = paste0(options$label, ':\\n', x), con = stderr())\n  paste0('```\\n', x, '\\n```')\n  cat(paste0(options$label, ':\\n', x, '\\n'), file = '", paste0(header, "_warnings.Rout"), "', append = TRUE)\n}\nknitr::knit_hooks$set(message = function(x, options) knit_hook_stderr(x, options), warning = function(x, options) knit_hook_stderr(x, options), error = function(x, options) stop(x))")
+    
+    knitr_chunk <- paste0("# first 'dev'-value should be suitable for output format specified in YAML metadata (html+word = raster graphics, pdf = vector graphics)\nknitr::opts_chunk$set(dev = 'png', dpi = 100, error = TRUE, warning = TRUE, message = TRUE, echo = TRUE)\nunlink('", paste0(header, "_warnings.Rout"), "')\n# knit_hook_stderr will redirect warnings and messages to stderr()\nknit_hook_stderr <- function(x, options) {\n  writeLines(text = paste0(options$label, ':\\n', x), con = stderr())\n  paste0('```\\n', x, '\\n```')\n  cat(paste0(options$label, ':\\n', x, '\\n'), file = '", paste0(header, "_warnings.Rout"), "', append = TRUE)\n}\nknitr::knit_hooks$set(message = function(x, options) knit_hook_stderr(x, options), warning = function(x, options) knit_hook_stderr(x, options), error = function(x, options) stop(x))")
+    
+    if (knitr_sidecar == TRUE) {
+      knitr_file <- paste0(tools::file_path_sans_ext(file), "_knitr_setup.R")
+      body_r <- paste0("source('", knitr_file,"')")
+      writeLines(knitr_chunk, con = knitr_file)
+    } else {
+      body_r <- knitr_chunk
+    }
+    
     setup_r <- paste0("\n#+ setup_", header, ", include = FALSE")
     setup_rmd_beginn <- paste0("\n```{r setup_", header, ", include=FALSE}")
     setup_rmd_end <- paste0("```")
-
-    note_r <- paste0("\n#' # Note\n#' - first 'dev'-value should be suitable for output format specified in YAML metadata (html+word = raster graphics, pdf = vector graphics)\n#' - knit_hook_stderr will redirect warnings and messages to stderr()")
-    note_rmd <- gsub("#' ", "", note_r)
-
+    
+    note_r <- paste0("\n#' # Note\n#' If you are using ggplot2 you will need to explicitly call 'print()' on each ggplot2 object in order to write the files (without spinning this script).")
+    note_rmd <- paste0("")
+    
     # check if file exists
     if (file.exists(file) == TRUE) {
       stop("File exists. Choose different file path.")
@@ -125,7 +136,7 @@ yaml_r_pdf <- paste0("#' ---\n#' title: '", header,"'\n#' author: '", Author, "'
       ),
       con = file)
     }
-
+    
     if (open == TRUE) {
       file.edit(file)
     }
