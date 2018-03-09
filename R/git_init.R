61c5da2f41088344cdbdd032ca5639393c497d96
Frederik Sachser <f.sachser@gmx.at>
2018-03-09 13:18:27 +0100

    git_init: using git2r

--- R/git_init.R
+++ R/git_init.R
@@ -6,9 +6,13 @@
 #' @author Frederik Sachser
 #' @export
 git_init <- function() {
-  system('git init')
-  system("git add .")
-  system("git commit -m 'v0.0.0.9000'")
-  system('git checkout -b devel')
+  repo <- git2r::init(path = ".")
+  git2r::add(repo, "*")
+  git2r::commit(repo, message = "v0.0.0.9000")
+  git2r::checkout(object = repo, branch = "devel", create = TRUE)
+#  system('git init')
+#  system("git add .")
+#  system("git commit -m 'v0.0.0.9000'")
+#  system('git checkout -b devel')
   message("commit master and checkout devel: done")
 }
